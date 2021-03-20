85b975cb78e9cf996719237c74a1b69bce85d583
Nicke <nikolas.syverod@gmail.com>
2021-03-20 16:52:25 +0200

    Added a submit button

--- src/components/Userinput.jsx
+++ src/components/Userinput.jsx
@@ -5,12 +5,15 @@ import { Inputlabel } from "./Inputlabel"
 export const Userinput = () => {
   return (
     <div className="flex justify-center w-full mt-12 h-32">
-      <form className="w-1/3 grid relative grid-cols-2 items-center ">
+      <form className="w-1/3 grid relative grid-cols-2 items-center">
         <Inputlabel htmlFor="weight" className="col-start-1 col-end-2 right-0 "/>
         <Input className="col-start-1 col-end-2 right-3 absolute items-center" id="weight" />
         <Inputlabel htmlFor="height" className="left-0 col-start-2 col-end-3 "/>
         <Input className="col-start-2 col-end-3 left-3 absolute " id="height" />
+        <input type="submit" value="submit" className="absolute bottom-1 left-72 rounded-xl p-1"/>
       </form>
     </div>
   );
 };
+
+
