9b9a1b662c9b995e4d9731263d5c64a452098c1f
Nicke <nikolas.syverod@gmail.com>
2021-03-22 01:42:03 +0200

    Added a hook to the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { Title } from "./components/Title";
 import { Userinput } from "./components/Userinput";
 import { Chart } from "./components/Chart"
+import { useState } from "react"
 
 const App = () => {
+  const [userInput, setUserInput] = useState([
+    { weight: null, height: null, date: null },
+  ]);
+
+  function bmiToChart(e) {
+    e.preventDefault();
+/*     const heightToMeter = height / 100;
+    const heightSquared = heightToMeter * heightToMeter;
+    const bmi = weight / heightSquared;
+    set
+    console.log(bmi); */
+  }
   return (
     <div className="h-screen w-screen bg-green-200 relative">
       <Title />
-      <Userinput />
+      <Userinput onClick={bmiToChart} setWeight={setUserInput} setWeight={(e) => setUserInput({...userInput, weight: parseFloat(e.target.value)})} setHeight={(e) => setUserInput({...userInput, height: parseFloat(e.target.value)})}/>
       <Chart />
     </div>
   );
 };
 
-export default App;
+export default App
--- src/components/Userinput.jsx
+++ src/components/Userinput.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
 import { Input } from "./Input";
-import { Inputlabel } from "./Inputlabel"
+import { Inputlabel } from "./Inputlabel";
 
-export const Userinput = () => {
+export const Userinput = ({ onClick, setWeight, setHeight }) => {
 
-  const [weight,  setWeight] = useState(null)
-  const [height,  setheight] = useState(null)  
 
-  function bmiToChart(e){
-    e.preventDefault()
-    console.log("jwi")
-  }
+
 
   return (
     <div className="flex justify-center w-full mt-12 h-32">
       <form className="w-1/3 grid relative grid-cols-2 items-center">
-        <Inputlabel htmlFor="weight" className="col-start-1 col-end-2 right-0 "/>
-        <Input className="col-start-1 col-end-2 right-3 absolute items-center" id="weight" onKeyUp={e => setWeight(e.target.value)} />
-        <Inputlabel htmlFor="height" className="left-0 col-start-2 col-end-3 "/>
-        <Input className="col-start-2 col-end-3 left-3 absolute " id="height" onKeyUp={e => setheight(e.target.value)}/>
-        <input type="submit" value="submit" className="absolute bottom-1 left-72 rounded-xl p-1" onClick={bmiToChart} disabled={weight === null || height === null}/>
+        <Inputlabel
+          htmlFor="weight"
+          className="col-start-1 col-end-2 right-0 "
+        />
+        <Input
+          className="col-start-1 col-end-2 right-3 absolute items-center"
+          id="weight"
+          onKeyUp={setWeight}
+        />
+        <Inputlabel
+          htmlFor="height"
+          className="left-0 col-start-2 col-end-3 "
+        />
+        <Input
+          className="col-start-2 col-end-3 left-3 absolute "
+          id="height"
+          onKeyUp={setHeight}
+        />
+        <input
+          type="submit"
+          value="submit"
+          className="absolute bottom-1 left-72 rounded-xl p-1"
+          onClick={onClick}
+/*           disabled={weight === null || height === null} */
+        />
       </form>
     </div>
   );
 };
-
-
-
-
-
-
-
