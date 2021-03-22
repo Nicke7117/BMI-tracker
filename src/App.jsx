76b90b9bc20d5f5956ba8141ec0acd8a7048d8fc
Nicke <nikolas.syverod@gmail.com>
2021-03-22 21:12:41 +0200

    User hook working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Title } from "./components/Title";
 import { Userinput } from "./components/Userinput";
-import { Chart } from "./components/Chart"
-import { useState } from "react"
+import { Chart } from "./components/Chart";
+import { useState } from "react";
 
 const App = () => {
   const [userInput, setUserInput] = useState([
-    { weight: null, height: null, date: null },
+    { weight: null, height: null, date: null, id: null },
   ]);
 
   function bmiToChart(e) {
     e.preventDefault();
-/*     const heightToMeter = height / 100;
+    setUserInput([...userInput]);
+    console.log(userInput);
+    /*     const heightToMeter = height / 100;
     const heightSquared = heightToMeter * heightToMeter;
     const bmi = weight / heightSquared;
     set
@@ -19,10 +21,19 @@ const App = () => {
   return (
     <div className="h-screen w-screen bg-green-200 relative">
       <Title />
-      <Userinput onClick={bmiToChart} setWeight={setUserInput} setWeight={(e) => setUserInput({...userInput, weight: parseFloat(e.target.value)})} setHeight={(e) => setUserInput({...userInput, height: parseFloat(e.target.value)})}/>
-      <Chart />
+      <Userinput
+        onClick={bmiToChart}
+        setWeight={setUserInput}
+        setWeight={(e) =>
+          setUserInput({ ...userInput, weight: parseFloat(e.target.value) })
+        }
+        setHeight={(e) =>
+          setUserInput({ ...userInput, height: parseFloat(e.target.value) })
+        }
+      />
+      <Chart onClick={bmiToChart} />
     </div>
   );
 };
 
-export default App
+export default App;
