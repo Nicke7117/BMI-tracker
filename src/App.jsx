4b2e2cac739d82b1abf1261d2f246cc157850489
Nicke <nikolas.syverod@gmail.com>
2021-03-23 23:55:30 +0200

    wrote code that transfers the state to app.js bmi state but it doesn't work correctly

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,20 @@ const App = () => {
     { weight: 55, height: 155, date: currentDate, id: 1 },
   ]);
 
-  function bmiToChart(e) {
-    e.preventDefault();
-    setBmi([...bmi, {weight: 55, height: 133, date: currentDate, id: 3}]);
-    console.log(bmi);
+  function bmiToChart(e, newBmi) {
+    e.preventDefault(
+      
+    )
+/*     setBmi([...bmi, newBmi])
+    console.log(bmi); */
   }
   return (
     <div className="h-screen w-screen bg-green-200 relative">
       <Title />
       <AddBMI 
-        onClick={bmiToChart}
+        onClick={(newBmi) => bmiToChart(newBmi)}
 />
-      <Chart onClick={bmiToChart} />
+      <Chart  />
     </div>
   );
 };
--- src/components/AddBMI.jsx
+++ src/components/AddBMI.jsx
@@ -4,8 +4,17 @@ import { Inputlabel } from "./Inputlabel";
 
 export const AddBMI = ({ onClick }) => {
   const [userData, setUserData] = useState({
-    weight: null, height: null, bmi: null, date: new Date().toISOString().split("T")[0]
-  })
+    weight: null,
+    height: null,
+    bmi: 22,
+    date: new Date().toISOString().split("T")[0],
+  });
+
+  function sendDataToApp(e){
+    e.preventDefault()
+    console.log(userData)
+    return userData;
+  }
 
   return (
     <div className="flex justify-center w-full mt-12 h-32">
@@ -17,7 +26,7 @@ export const AddBMI = ({ onClick }) => {
         <Input
           className="col-start-1 col-end-2 right-3 absolute items-center"
           id="weight"
-          onKeyUp={(e) => setUserData({...userData, weight: e.target.value})}
+          onKeyUp={(e) => setUserData({ ...userData, weight: e.target.value })}
         />
         <Inputlabel
           htmlFor="height"
@@ -26,18 +35,17 @@ export const AddBMI = ({ onClick }) => {
         <Input
           className="col-start-2 col-end-3 left-3 absolute "
           id="height"
-          onKeyUp={(e) => setUserData({...userData, height: e.target.value})}
+          onKeyUp={(e) => setUserData({ ...userData, height: e.target.value })}
         />
         <input
           type="submit"
           value="submit"
           className="absolute bottom-1 left-72 rounded-xl p-1"
-          onClick={onClick}
-          disabled={weight === null || height === null}
+          onClick={() => onClick(sendDataToApp)}
+          disabled={userData.weight === null || userData.height === null}
         />
       </form>
     </div>
   );
 };
-
 
