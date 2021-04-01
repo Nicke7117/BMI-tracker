961baa78f25940b8849ddfc740e0ec03755ee371
Nicke <nikolas.syverod@gmail.com>
2021-04-01 20:57:45 +0300

    I added a useEffect to save the bmiData after it has been set

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Title } from "./components/Title";
 import { Form } from "./components/Form";
 import { Chart } from "./components/Chart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const App = () => {
   const [bmiData, setbmiData] = useState([]);
+
   function bmiDataToHook(newbmiData) {
     setbmiData([...bmiData, newbmiData]);
-    localStorage.setItem("Bmi data", JSON.stringify(bmiData));
   }
+
+  useEffect(() => {
+    localStorage.setItem("Bmi data", JSON.stringify(bmiData));
+  });
 
   return (
     <div className="bg-pink-300 w-screen h-screen">
