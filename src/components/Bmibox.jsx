010a12bd171d24e6003f62f0f66c33706b9cbe50
Nicke <nikolas.syverod@gmail.com>
2021-04-03 16:57:06 +0300

    Created the box where every bmi will be added

---
+++ src/components/Bmibox.jsx
@@ -0,0 +1,7 @@
+export const Bmibox = () => {
+  return (
+    <div className="flex justify-center h-80 mt-7 pb-7">
+      <div className="bg-blue-200 w-5/12 rounded-xl"></div>
+    </div>
+  );
+};
