608a3615cbb381a7df8d0b6dda10338b408a6f6d
Nicke <nikolas.syverod@gmail.com>
2021-04-05 23:08:08 +0300

    The grid for 4 bmis is ready, impovements will be made

--- src/components/Bmibox.jsx
+++ src/components/Bmibox.jsx
@@ -1,7 +1,11 @@
-export const Bmibox = () => {
+import { Bmi } from "./Bmi";
+
+export const Bmibox = ({ bmiData }) => {
   return (
     <div className="flex justify-center h-80 mt-7 pb-7">
-      <div className="bg-blue-200 w-5/12 rounded-xl"></div>
+      <div className="bg-blue-200 w-5/12 gap-4 rounded-xl grid grid-cols-2 grid-rows-2 overflow-y-auto">
+        <Bmi />
+      </div>
     </div>
   );
 };
