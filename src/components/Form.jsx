9044b7fc457c579c1796f26e355bb9e5d2fa834e
Nicke <nikolas.syverod@gmail.com>
2021-04-05 23:10:15 +0300

    Removed import React

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Input } from "./Input";
 import { Inputlabel } from "./Inputlabel";
 import { v4 as uuidv4 } from "uuid";
