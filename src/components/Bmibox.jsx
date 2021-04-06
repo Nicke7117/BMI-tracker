6893babc9dbd4d3e0dbef52ed61c3d40637cf20c
Nicke <nikolas.syverod@gmail.com>
2021-04-06 21:08:57 +0300

    Added key and id prop

--- src/components/Bmibox.jsx
+++ src/components/Bmibox.jsx
@@ -4,7 +4,7 @@ export const Bmibox = ({ bmiData }) => {
   return (
     <div className="flex justify-center h-80 mt-7 pb-7">
       <div className={`bg-blue-200 w-5/12 pt-2 rounded-xl grid ${bmiData.length <= 2 ? "grid-rows-2 " : ""} grid-cols-2 ${bmiData.length > 4 ? "overflow-y-auto" : ""}`}>
-      {bmiData.length > 0 ? bmiData.map(bmi => <Bmi bmiData={bmi}/>) : ""}
+      {bmiData.length > 0 ? bmiData.map(bmi => <Bmi bmiData={bmi} key={bmi.id} id={bmi.id}/>) : ""}
       </div>
     </div>
   );
