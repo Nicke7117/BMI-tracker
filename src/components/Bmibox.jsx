78cb6e59a13206b69e97bf9322d921ad69c1147a
Nicke <nikolas.syverod@gmail.com>
2021-04-06 11:52:38 +0300

    Created a function that returns the level of bmi

--- src/components/Bmibox.jsx
+++ src/components/Bmibox.jsx
@@ -3,8 +3,8 @@ import { Bmi } from "./Bmi";
 export const Bmibox = ({ bmiData }) => {
   return (
     <div className="flex justify-center h-80 mt-7 pb-7">
-      <div className="bg-blue-200 w-5/12 gap-4 rounded-xl grid grid-cols-2 grid-rows-2 overflow-y-auto">
-        <Bmi />
+      <div className={`bg-blue-200 w-5/12 pt-2 rounded-xl grid ${bmiData.length <= 2 ? "grid-rows-2 " : ""} grid-cols-2 ${bmiData.length > 4 ? "overflow-y-auto" : ""}`}>
+      {bmiData.length > 0 ? bmiData.map(bmi => <Bmi bmiData={bmi}/>) : ""}
       </div>
     </div>
   );
