79cd07e1aaeb423006ea3716a93f0233c7453a62
Nicke <nikolas.syverod@gmail.com>
2021-04-07 17:26:16 +0300

    Changed the text

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,9 @@ export const Title = () => {
   return (
     <>
       <h1 className="text-center pt-10 text-5xl font-black">BMI Tracker</h1>
-      <p className="text-center pt-5 text-2xl font-extrabold" >Save your BMI into a graph for up to 7 days!</p>
+      <p className="text-center pt-5 text-2xl font-extrabold">
+        Keep track of your BMI!
+      </p>
     </>
   );
 };
